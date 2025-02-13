2aa2a0f1fbf1f3f1099e9f197389c63099ca35d1
adarsh-goswamii <my@personal>
2025-02-13 12:08:36 +0530

    updated

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ document = {
 }
 
 # Adding document to an index
-es.index(index=book_index, body=document)
+book_response = es.index(index=book_index, body=document)
 
 # default mapping
 mapping = es.indices.get_mapping(index=book_index)
@@ -74,3 +74,59 @@ es.indices.create(index="nested_object_index", mappings={
         }
     }
 })
+
+# Text search type - text
+es.indices.create("text_index", mappings={
+    "properties": {
+        "email_body": {"type": "text"}
+    }
+})
+
+email = "this is an email body"
+es.index(index="text_index", body={"email_body": email})
+
+# Text search type - completion
+es.indices.create("completion_index", mappings={
+    "properties": {
+        "city": {"type": "completion"}
+    }
+})
+city_options = ["Lucknow", "Agra", "Kanpur"]
+es.index(index="completion_index", body={"city": { "input": city_options }})
+
+# delete document
+es.delete(index=book_index, id='1') # Note if id is not present in the index it will throw error.
+
+# Get document
+es.get(index=book_index, id='1')
+
+# Count document
+es.count(index=book_index, query="user:adarsh")
+
+# Exists - index
+es.indices.exists(index=book_index)
+
+# Exists - document
+es.exists(index=book_index, id='1')
+
+# Update document - existing field
+response = es.update(index=book_index, id=book_response.get('_id', ""),
+          script={"source": "ctx._source.title = params.title", "params": {"title": "Updated Book Title"}})
+
+# Update document - updating a document using doc (patch operation)
+response = es.update(index=book_index, id=book_response.get('_id', ""),
+          doc={ "title": "Learn elasticsearch" })
+
+# Update document - adding a new field
+response = es.update(index=book_index, id=book_response.get('_id', ""),
+          script={"source": "ctx._source.description = params.desc", "params": {"desc": "Book description"}})
+
+# Update document - removing a field
+response = es.update(index=book_index, id=book_response.get('_id', ""),
+          script={"source": "ctx._source.remove('description')"})
+
+# Update document - add document if no such id is found
+response = es.update(index=book_index, id="wrong_id",
+          doc={ "title": "Learn elasticsearch" }, doc_as_upsert=True)
+
+print(es.get(index=book_index, id=response.get('_id')))
